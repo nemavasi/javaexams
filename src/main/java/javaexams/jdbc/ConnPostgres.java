e366383c1692a56f4e8dcd12a56d39b820bfdc6a
Шалыгин Дмитрий <dshalygin@c-i-p.ru>
2019-02-07 17:11:45 +0300

    jdbc

---
+++ src/main/java/javaexams/jdbc/ConnPostgres.java
@@ -0,0 +1,24 @@
+package javaexams.jdbc;
+
+import java.sql.*;
+
+/**
+ * Created by dshalygin on 2/7/19.
+ */
+public class ConnPostgres {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        Connection connection = null;
+        connection = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/testing_db","nemavasi", "0");
+
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("select 42");
+        rs.next();
+        int v = rs.getInt(1);
+        System.out.println(v);
+
+        connection.close();
+
+    }
+}
