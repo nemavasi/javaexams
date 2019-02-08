6e0ad44a34cd7e6a75c8c4be46f0689d824eada7
nemavasi <thatbook2@gmail.com>
2019-02-08 08:25:41 +0300

    future

---
+++ src/main/java/javaexams/concurrency/TFuture.java
@@ -0,0 +1,36 @@
+package javaexams.concurrency;
+
+import javaexams.BaseCheck;
+
+import java.util.concurrent.*;
+
+public class TFuture extends BaseCheck {
+    public static void main(String[] args) {
+        new TFuture().go();
+    }
+
+    @Override
+    public void go() {
+
+        p(Thread.currentThread().getName());
+
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        Callable<String> c = new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                p(Thread.currentThread().getName());
+                Thread.sleep(3000);
+                return "ok";
+            }
+        };
+        Future<String> f = es.submit(c);
+        try {
+            p(f.get());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        es.shutdown();
+
+    }
+}
