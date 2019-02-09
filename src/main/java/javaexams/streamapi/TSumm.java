2b9e35de54764f56f470e4484cdc1af3a81f396e
nemavasi <thatbook2@gmail.com>
2019-02-09 09:28:22 +0300

    stream sum

---
+++ src/main/java/javaexams/streamapi/TSumm.java
@@ -0,0 +1,22 @@
+package javaexams.streamapi;
+
+import javaexams.BaseCheck;
+
+import java.util.function.ToDoubleFunction;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class TSumm extends BaseCheck {
+    public static void main(String[] args) {
+        new TSumm().go();
+    }
+
+    @Override
+    public void go() {
+
+        Double sum = IntStream.of(0, 6 ,7).asDoubleStream()
+                .average().getAsDouble()
+                ;
+        p(sum);
+    }
+}
