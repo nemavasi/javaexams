d67eb9a8ed696261d62f94b1462a66d7f79e886b
nemavasi <thatbook2@gmail.com>
2019-02-09 15:04:04 +0300

    process

--- src/main/java/javaexams/others/TProcess.java
+++ src/main/java/javaexams/others/TProcess.java
@@ -1,4 +1,4 @@
-package javaexams.net;
+package javaexams.others;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 
-public class IsReachable
+public class TProcess
 {
 
 
