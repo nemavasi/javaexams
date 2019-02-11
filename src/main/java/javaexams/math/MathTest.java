6dbdab7077568c18088ba2bdbade0482562eb3dc
Шалыгин Дмитрий <dshalygin@c-i-p.ru>
2019-02-11 10:31:21 +0300

    math

--- src/main/java/javaexams/math/MathTest.java
+++ src/main/java/javaexams/math/MathTest.java
@@ -1,11 +1,15 @@
-package math;
+package javaexams.math;
 
 public class MathTest {
     public static void main(String... args) {
         System.out.println((-7 % 5) > (7 % -5));
         System.out.println(Math.abs(Math.ceil(-2.5)) < Math.ceil(Math.abs(-2.5)));
+        System.out.println((-7 % 5) );
+        System.out.println((7 % -5));
     }
 
     //false
     //true
+    //-2
+    //2
 }
