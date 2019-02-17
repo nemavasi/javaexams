b1e9d77137e05ad026f6a6bf236f284bffba5c17
nemavasi <thatbook2@gmail.com>
2019-02-17 15:29:19 +0300

    overflow

---
+++ src/main/java/javaexams/others/TOverflow.java
@@ -0,0 +1,10 @@
+package javaexams.others;
+
+public class TOverflow {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE+1==Integer.MIN_VALUE);
+        System.out.println(Integer.MIN_VALUE-1==Integer.MAX_VALUE);
+//        true
+//        true
+    }
+}
