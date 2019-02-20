c0793b4576c583de940c6a33a2e4a971b595e0a7
nemavasi <thatbook2@gmail.com>
2019-02-20 11:43:00 +0300

    string join

---
+++ src/main/java/javaexams/others/TStringJoin.java
@@ -0,0 +1,8 @@
+package javaexams.others;
+
+public class TStringJoin {
+    public static void main(String[] args) {
+        System.out.println(String.join(";", "qwe", "rty", "uyt"));
+        // qwe;rty;uyt
+    }
+}
