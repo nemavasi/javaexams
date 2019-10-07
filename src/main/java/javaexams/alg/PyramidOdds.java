e722552b131c289e35a637499dd9482349e3565d
dvshalygin <thatbook2@gmail.com>
2019-10-07 08:00:56 +0300

    not terminated ExecutorService

--- src/main/java/javaexams/alg/PyramidOdds.java
+++ src/main/java/javaexams/alg/PyramidOdds.java
@@ -14,6 +14,6 @@ public class PyramidOdds {
     }
 
     private static int foo(int n) {
-        return 0;
+        return n * n * n; //математически выводиться через арифмитическую прогрессию
     }
 }
