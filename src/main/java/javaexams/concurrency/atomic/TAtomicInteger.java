bda519ff338d19ddc733cfe3ef72b9744fa5c0b6
dvshalygin <thatbook2@gmail.com>
2019-10-08 08:06:35 +0300

    AtomicInteger

--- src/main/java/javaexams/concurrency/atomic/TAtomicInteger.java
+++ src/main/java/javaexams/concurrency/atomic/TAtomicInteger.java
@@ -10,17 +10,20 @@ public class TAtomicInteger {
     public static void main(String[] args) throws Exception {
 
         AtomicInteger atomicInt = new AtomicInteger(0);
+        AtomicInteger atomicInt2 = new AtomicInteger(0);
         ExecutorService executor = Executors.newFixedThreadPool(2);
 
         IntStream.range(0, 1000)
                 .forEach(newValue -> {
                     Runnable task = () ->
-                            atomicInt.accumulateAndGet(newValue, (prev, next) -> prev + next);
+                            atomicInt.accumulateAndGet(newValue, (prev, next) -> prev + next);     //IntBinaryOperator
+                            atomicInt2.updateAndGet((oldValue) -> oldValue + newValue);            //IntUnaryOperator
                     executor.submit(task);
                 });
 
         TimeUnit.SECONDS.sleep(3);  //ждем когда все потоки отработают - иначе можно получить результат промежуточный
         executor.shutdown();
         System.out.println(atomicInt.get());    // => 499500
+        System.out.println(atomicInt2.get());    // => 499500
     }
 }
