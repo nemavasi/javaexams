7a9ef6fe8d5776d8913d894d51686674c8c04eb7
dvshalygin <thatbook2@gmail.com>
2019-10-28 17:09:37 +0300

    bard

--- src/main/java/javaexams/concurrency/Bard2.java
+++ src/main/java/javaexams/concurrency/Bard2.java
@@ -9,23 +9,66 @@ package javaexams.concurrency;
 //        классом Exchanger(иначе как барды будут друг другу что-то говорить?) и классом Phaser (иначе как барды поймут,
 //        когда нужно делиться словом?)
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.Phaser;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class Bard2 {
 
-    private String song;
+    private List<String> song;
     private String name;
+    private AtomicReference<Bard2> speaker;
+    private Phaser phaser;
+    private Exchanger<String> exchanger;
+    private List<String> newSong = new ArrayList<>();
 
     //private
 
-    public Bard2(String song, String name) {
+    public Bard2(List<String>  song, String name, AtomicReference<Bard2> speaker, Phaser phaser, Exchanger<String> exchanger) {
         this.song = song;
         this.name = name;
+        this.speaker = speaker;
+        this.phaser = phaser;
+        this.exchanger = exchanger;
     }
 
-    public void tellWordsToOthers(){
-
+    public void tellWordsToOthers () throws Exception{
+        for (int i=0; i < 2; i++) {
+            phaser.arriveAndAwaitAdvance();
+            if (speaker.get() != this) {
+                speaker.set(this);
+            }
+            phaser.arriveAndAwaitAdvance();
+            if (speaker.get() == this) {
+                exchanger.exchange(song.get(i));
+                exchanger.exchange(song.get(i));
+                exchanger.exchange(song.get(i));
+            } else {
+                String wordFromOther = exchanger.exchange(null);
+                newSong.add(wordFromOther);
+            }
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        AtomicReference<Bard2> speaker = new AtomicReference<>();
+        Phaser phaser = new Phaser(4);
+        Exchanger<String> exchanger = new Exchanger<>();
+
+        List<Bard2> bards = new ArrayList<>();
+        bards.add(new Bard2(Arrays.asList("Hello", "world", "Java", "People"), "Jack", speaker, phaser, exchanger));
+        bards.add(new Bard2(Arrays.asList("Hello", "world", "Java", "People"), "Jack", speaker, phaser, exchanger));
+        bards.add(new Bard2(Arrays.asList("Hello", "world", "Java", "People"), "Jack", speaker, phaser, exchanger));
+        bards.add(new Bard2(Arrays.asList("Hello", "world", "Java", "People"), "Jack", speaker, phaser, exchanger));
+
+        for(Bard2 bard : bards){
+            bard.tellWordsToOthers();
+        }
+
+        System.out.println(bards.get(0).newSong);
 
     }
 }
