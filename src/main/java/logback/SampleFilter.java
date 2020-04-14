0311c2e9a174228cdf409331f9e058ddfb853d96
shalygindv <shalygindv@oooinex.ru>
2020-04-14 17:41:50 +0300

    logback.xml

--- src/main/java/logback/SampleFilter.java
+++ src/main/java/logback/SampleFilter.java
@@ -4,11 +4,14 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
 
+import java.util.Arrays;
+
 public class SampleFilter extends Filter<ILoggingEvent> {
 
   @Override
   public FilterReply decide(ILoggingEvent event) {
     System.out.println(event.getFormattedMessage());
+    System.out.println(Arrays.toString(event.getArgumentArray()));
     if (event.getFormattedMessage().contains("Handbook")) {
       return FilterReply.ACCEPT;
     } else {
