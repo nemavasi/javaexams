2b4063a805c4f4c3065c20b2856b64ac6289c418
shalygindv <shalygindv@oooinex.ru>
2020-04-14 16:52:37 +0300

    logback.xml

--- src/main/java/logback/TstLogback.java
+++ src/main/java/logback/TstLogback.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 public class TstLogback {
 
   private static final Logger logger = LoggerFactory.getLogger(TstLogback.class);
+  private static final Logger logger2 = LoggerFactory.getLogger("ru");
+
 
   public static void main(String[] args) {
     logger.trace("trace Example log from {}", TstLogback.class.getSimpleName());
@@ -13,5 +15,7 @@ public class TstLogback {
     logger.info("info Example log from {}", TstLogback.class.getSimpleName());
     logger.warn("warn Example log from {}", TstLogback.class.getSimpleName());
     logger.error("error Example log from {}", TstLogback.class.getSimpleName());
+
+    logger2.info("info Example log from {}", TstLogback.class.getSimpleName());
   }
 }
