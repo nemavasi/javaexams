3d1fff4b55c4dffe783f4fe54276ffa5919b936b
shalygindv <shalygindv@oooinex.ru>
2020-05-20 14:31:49 +0300

    Numeric Overflow in Expression

--- src/main/java/javaexams/math/NumericOverflowInExpression.java
+++ src/main/java/javaexams/math/NumericOverflowInExpression.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 public class NumericOverflowInExpression {
   public static void main(String[] args) throws Exception {
+    System.out.println(30 * 24 * 60 * 60 * 1000);
+    System.out.println(30L * 24 * 60 * 60 * 1000);
     System.out.println(new SimpleDateFormat("dd.MM.yyyy").parse("22.05.2020").getTime() + 30 * 24 * 60 * 60 * 1000 > new Date().getTime());
     System.out.println(new SimpleDateFormat("dd.MM.yyyy").parse("22.05.2020").getTime() + 30L * 24 * 60 * 60 * 1000 > new Date().getTime());
   }
