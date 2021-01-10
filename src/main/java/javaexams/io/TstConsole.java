bf8a71e3b511bf1494de548cb25aa37c00f15ea3
shalygindv <shalygindv@oooinex.ru>
2021-01-10 19:08:43 +0300

    io - console

--- src/main/java/javaexams/io/TstConsole.java
+++ src/main/java/javaexams/io/TstConsole.java
@@ -4,6 +4,7 @@ import java.io.Console;
 
 public class TstConsole {
   public static void main(String[] args) {
+    /*If the virtual machine is started automatically, for example by a background job scheduler, then it will typically not have a console.*/
     Console console = System.console(); //будет null если запускать класс не из консоли
     char[] pass = console.readPassword();
     System.out.println(new String(pass));
