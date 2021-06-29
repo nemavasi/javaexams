4a92e4ae0794703b6c905e4f96a72e54102c41a4
dmitrii_shalygin <dmitrii_shalygin@epam.com>
2021-06-29 09:34:56 +0300

    default booleans

---
+++ src/main/java/javaexams/core/DefaultBoolean.java
@@ -0,0 +1,13 @@
+package javaexams.core;
+
+public class DefaultBoolean {
+
+    private boolean a;
+    private Boolean b;
+
+    public static void main(String[] args) {
+        System.out.println(new DefaultBoolean().a);
+        System.out.println(new DefaultBoolean().b);
+    }
+
+}
