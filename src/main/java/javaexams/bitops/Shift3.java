8ff5abda7c6e1d25a7cb629b282e2181d4b35589
dmitrii_shalygin <dmitrii_shalygin@epam.com>
2021-07-27 20:09:29 +0300

    reformat

--- src/main/java/javaexams/bitops/Shift3.java
+++ src/main/java/javaexams/bitops/Shift3.java
@@ -4,7 +4,7 @@ public class Shift3 {
     public static void main(String[] args) {
         long start = 11917478287L; //Long.MAX_VALUE-11;
         for (long i = start; i < start + 10; i++ ) {
-            System.out.println(String.format("%d %d %d", i, i >>> 32, i ^ (i>>>32)));
+            System.out.printf("%d %d %d%n", i, i >>> 32, i ^ (i>>>32));
         }
     }
 }
