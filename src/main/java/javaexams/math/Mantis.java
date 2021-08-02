2eb5925576f9150168cdbf1cdf54248b4fcc8e0f
dmitrii_shalygin <dmitrii_shalygin@epam.com>
2021-08-02 10:04:22 +0300

    mantissa

---
+++ src/main/java/javaexams/math/Mantis.java
@@ -0,0 +1,11 @@
+package javaexams.math;
+
+public class Mantis {
+
+    public static void main(String[] args) {
+        System.out.println(3.14159e-5);    //3.14159E-5
+        System.out.println(0xFFP-0);       //255.0
+        System.out.println(255.0/(2*2*2)); //31.875
+        System.out.println(0xFFP-3);       //31.875
+    }
+}
