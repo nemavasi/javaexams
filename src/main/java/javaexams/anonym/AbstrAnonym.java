7a22526352eb9a09515f1cde3d2e73fe2c278d30
dmitrii_shalygin <dmitrii_shalygin@epam.com>
2021-08-21 07:39:17 +0300

    non-denotable type

--- src/main/java/javaexams/anonym/AbstrAnonym.java
+++ src/main/java/javaexams/anonym/AbstrAnonym.java
@@ -4,10 +4,18 @@ public class AbstrAnonym {
 
     public static void main(String[] args) {
         Foo foo = new Foo() {
-            void test(){}
+            void test(){
+                System.out.println("test");
+            }
         };
         //foo.test(); it does not compile because it is Foo class, and Foo class does not have test() method
 
+        var foo2 = new Foo() {
+            void test(){
+                System.out.println("test");
+            }
+        };
+        foo2.test(); //because foo2 is non-denotable type, it has test() method
     }
 }
 
