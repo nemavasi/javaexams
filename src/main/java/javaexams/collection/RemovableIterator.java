e2769ae0b11475b41b666e3d46e5a60dcbf08cd4
dmitrii_shalygin <dmitrii_shalygin@epam.com>
2021-09-22 16:01:03 +0300

    remove with iterator

--- src/main/java/javaexams/collection/RemovableIterator.java
+++ src/main/java/javaexams/collection/RemovableIterator.java
@@ -15,5 +15,12 @@ public class RemovableIterator {
         listIterator.next();
         listIterator.remove();
         System.out.println(baseList);                  // [1, 2, 4, 5]
+        listIterator.remove();                         // "main" java.lang.IllegalStateException
+
+//        An iterator for a list of length n has n+1 possible cursor positions (between elements)
+//        Note that the remove() and set(Object) methods are not defined in terms of the cursor position;
+//        they are defined to operate on the last element returned by a call to next() or previous().
+
+//        This method can be called only once per call to next()
     }
 }
