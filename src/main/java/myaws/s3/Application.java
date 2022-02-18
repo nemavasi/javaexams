2fd24afa7cc76f0d5917a83de5aacb3cbac37360
dmitrii_shalygin <dmitrii_shalygin@epam.com>
2022-02-18 08:42:17 +0300

    aws s3

--- src/main/java/myaws/s3/Application.java
+++ src/main/java/myaws/s3/Application.java
@@ -4,12 +4,26 @@ import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 //    private static final String AWS_ACCESS_KEY = "AWS_ACCESS_KEY_ID";
 //    private static final String AWS_SECRET_ACCESS_KEY = "AWS_SECRET_ACCESS_KEY";
     private static final String BUCKET_NAME = "sdfgfghgffgytyv";
+    private static final String FILE_NAME1 = "example.txt";
+    private static final String FILE_NAME2 = "newname.txt";
+    private static final String FILE_NAME3 = "downlodedFile.txt";
+    private static final URL FILE_URL = Application.class.getClassLoader().getResource(FILE_NAME1);
+    //private static final String DOWNLOADED_FILE_URL = Application.class.getClassLoader().getName();
 
     private final AmazonS3Client s3Client;
 
@@ -17,16 +31,34 @@ public class Application {
         this.s3Client = s3Client;
     }
 
-    public void createBucket(String name){
+    public void createBucket(String bucketName){
         try {
-            CreateBucketRequest createBucketRequest = new CreateBucketRequest(name);
+            CreateBucketRequest createBucketRequest = new CreateBucketRequest(bucketName);
             s3Client.createBucket(createBucketRequest);
         } catch (Exception e) {
-
+            log.error("Error in createBucket", e);
         }
     }
 
-    public static void main(String[] args) {
+    public void uploadFile(String bucketName, String key, File file){
+        try {
+            PutObjectRequest putBucketRequest = new PutObjectRequest(bucketName, key, file);
+            s3Client.putObject(putBucketRequest);
+        } catch (Exception e) {
+            log.error("Error in uploadFile", e);
+        }
+    }
+
+    public void downloadFile(String bucketName, String key, File downloadedFile) {
+        try {
+            GetObjectRequest getBucketRequest = new GetObjectRequest(bucketName, key);
+            s3Client.getObject(getBucketRequest, downloadedFile);
+        } catch (Exception e) {
+            log.error("Error in downloadFile", e);
+        }
+    }
+
+    public static void main(String[] args) throws URISyntaxException {
         String region = Regions.EU_CENTRAL_1.getName();
 
 //        String accessKey = System.getenv(AWS_ACCESS_KEY);
@@ -38,6 +70,13 @@ public class Application {
             .build();
 
         Application app = new Application(amazonS3Client);
-        app.createBucket(BUCKET_NAME);
+        //app.createBucket(BUCKET_NAME);
+
+//        File file = new File(FILE_URL.toURI());
+//        app.uploadFile(BUCKET_NAME, FILE_NAME2, file);
+
+       // log.info("dff",FILE_URL.getPath() );
+
+        app.downloadFile(BUCKET_NAME, FILE_NAME2, new File(FILE_URL.getPath()+".downloaded"));
     }
 }
